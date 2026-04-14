de1477f4f1c2d423ab01d5b0d6512d075dffae17
Khawla Zarrami <zarramikhaoula@gmail.com>
2026-04-14 13:04:12 +0100

    Add Workspace projects

---
+++ MyToDos/MyToDos/MyToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosApp.swift
+//  MyToDos
+//
+//  Created by Khawla Zarrami on 14/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RelationShips in SwiftData/RelationShips in SwiftData/RelationShips_in_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelationShips_in_SwiftDataApp.swift
+//  RelationShips in SwiftData
+//
+//  Created by Khawla Zarrami on 14/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct RelationShips_in_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
